fd380e98e05a590679a7a7d131920d3d57ab0c0e
Sang sakarin <sang_sakarin@outlook.com>
2020-05-17 21:49:12 +0700

    feat: method gererate QR 30 & QR cs

--- scb_payment/qrcode_payment.py
+++ scb_payment/qrcode_payment.py
@@ -55,3 +55,30 @@ class QRCodePayment(Authorization):
             raise SCBPaymentError("{0} {1}".format(response['status']['code'], response['status']['description']))
 
         return response['data']
+
+    @check_in_kwargs(["amount", "invoice", "ref1", "ref2", "ref3"])
+    def gererate_qr_30_qr_cs(self, amount=None, invoice=None, description="", expiry_time=60, user_definded="", ref1=None, ref2=None, ref3=None):
+        url = self._get_path("PAYMENT_QRCODE_PATH")
+
+        payload = {}
+        payload["qrType"] = QRType.QR_30_QR_CS
+        payload["amount"] = amount
+        payload["ppType"] = "BILLERID"
+        payload["ppId"] = self.biller
+        payload["ref1"] = ref1
+        payload["ref2"] = ref2
+        payload["ref3"] = ref3
+        payload["csExtExpiryTime"] = expiry_time
+        payload["csNote"] = description
+        payload["csUserDefined"] = user_definded
+        payload["invoice"] = invoice
+        payload["merchantId"] = self.merchant
+        payload["terminalId"] = self.terminal
+        payload = json.dumps(payload)
+
+        response = basic_request('POST', url, headers=self._get_headers(), payload=payload)
+
+        if response['status']['code'] != BusinessCode.SUCCESS:
+            raise SCBPaymentError("{0} {1}".format(response['status']['code'], response['status']['description']))
+
+        return response['data']
